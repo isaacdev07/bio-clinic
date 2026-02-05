38257dd6fb25d480c07b6f25e754fcad9f7d5fde
Isaac Santos <isaacsantos7953@gmail.com>
2026-02-04 22:43:58 -0300

    Add comment for CORS configuration in WebConfig
    Added a comment indicating that CORS configuration is okay.

--- src/main/java/com/bio/clinic/config/WebConfig.java
+++ src/main/java/com/bio/clinic/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+    // cors config ok
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
